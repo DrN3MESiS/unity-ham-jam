b9b17e43b95cbf27d5ed6194dffe3f45cff17abe
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-09-24 00:00:24 -0500

    Block Generator

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,24 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour
+public class Block : ScriptableObject
 {
-    public int unitWidth = 100;
-    public int[] valleysEntityAmount = new int[2] { 0, 6 };
-    public int[] mountainEntityAmount = new int[2] { 0, 8 };
-    public int[] fuelEntityAmount = new int[2] { 2, 3 };
-    // Start is called before the first frame update
+    /* Properties */
+    private int maxWidth = 100;
 
-    public List<Object> Entities = new List<Object>();
+    private int[] valleysEntityAmount = new int[2] { 0, 6 };
+    private int[] mountainEntityAmount = new int[2] { 0, 8 };
+    private int[] fuelEntityAmount = new int[2] { 2, 3 };
 
+    /*  */
+    public List<Mountain> Entities = new List<Mountain>();
+    private int unitsLeft = 100;
+
+    /* UNITY METHODS */
     void Start()
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
 
+    }
+
+    /* CLASS METHODS */
+
+    public int Generate()
+    {
+        /*First Pass*/
+        while (true)
+        {
+            Mountain testMountain = ScriptableObject.CreateInstance<Mountain>();
+            if (unitsLeft - testMountain.actualWidth < 0)
+            {
+                break;
+            }
+
+            Entities.Add(testMountain);
+            unitsLeft -= testMountain.actualWidth;
+        }
+
+        Debug.Log("unitsLeft on Block after Generation: " + unitsLeft);
+        Debug.Log("Mountains Generated: " + Entities.Count);
+
+        return 0;
+    }
+
+    /* Getters and Setters */
+    public int GetMaxWidth()
+    {
+        return maxWidth;
+    }
+    public int GetMinValleyAmount()
+    {
+        return valleysEntityAmount[0];
+    }
+    public int GetMaxValleyAmount()
+    {
+        return valleysEntityAmount[1];
+    }
+    public int GetMinMountainAmount()
+    {
+        return mountainEntityAmount[0];
+    }
+    public int GetMaxMountainAmount()
+    {
+        return mountainEntityAmount[1];
+    }
+    public int GetMinFuelAmount()
+    {
+        return fuelEntityAmount[0];
+    }
+    public int GetMaxFuelAmount()
+    {
+        return fuelEntityAmount[1];
     }
 }
---
+++ Assets/Scripts/Random.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static class IntUtil
+{
+    private static Random random;
+
+    private static void Init()
+    {
+        if (random == null) random = new Random();
+    }
+
+    public static int Random(int min, int max)
+    {
+        Init();
+        return random.Next(min, max);
+    }
+}
