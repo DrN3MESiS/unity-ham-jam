cab6a384869f5df5fa694a0e4f74072c6842e512
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-09-28 17:32:10 -0500

    Update

--- Assets/Scripts/Valley.cs
+++ Assets/Scripts/Valley.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Valley : MonoBehaviour
 {
-    public float[] width = new float[2]{3,5};
+    public static float[] width = new float[2] { 3, 5 };
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
