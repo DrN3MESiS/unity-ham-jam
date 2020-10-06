b43847c2ff75ba9c9195c8ab18738bf8aa5c10b0
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-10-05 20:37:38 -0500

    .

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,9 +7,9 @@ public class Block : MonoBehaviour
     /* Properties */
     private int maxWidth = 100;
 
-    private int[] valleysEntityAmount = new int[2] { 0, 6 };
-    private int[] mountainEntityAmount = new int[2] { 0, 8 };
-    private int[] fuelEntityAmount = new int[2] { 2, 3 };
+    private static int[] valleysEntityAmount = new int[2] { 0, 6 };
+    private static int[] mountainEntityAmount = new int[2] { 0, 8 };
+    private static int[] fuelEntityAmount = new int[2] { 2, 3 };
 
     /*  */
     public List<GameObject> Entities = new List<GameObject>();
@@ -21,25 +21,19 @@ public class Block : MonoBehaviour
     void Start()
     {
 
-    }
-    void Update()
-    {
+        Debug.Log("[Block.Start()] >> " + "Generated Block");
 
-    }
 
-    /* CLASS METHODS */
-
-    public int Generate()
-    {
-        /*First Pass*/
         while (true)
         {
-            GameObject testMountain = new GameObject();
+            GameObject testMountain = new GameObject("Mountain_");
             Mountain _mountainScript = testMountain.AddComponent<Mountain>();
+
             if (unitsLeft - _mountainScript.actualWidth < 0)
             {
                 break;
             }
+
 
             if (_mountainScript.topIsValley)
             {
@@ -49,42 +43,14 @@ public class Block : MonoBehaviour
             Entities.Add(testMountain);
             Scripts.Add(_mountainScript);
             unitsLeft -= _mountainScript.actualWidth;
+
         }
 
-        Debug.Log("unitsLeft on Block after Generation: " + unitsLeft);
         Debug.Log("Mountains Generated: " + Entities.Count);
         Debug.Log("Valley: " + CountOfValleys);
+    }
+    void Update()
+    {
 
-        return 0;
-    }
-
-    /* Getters and Setters */
-    public int GetMaxWidth()
-    {
-        return maxWidth;
-    }
-    public int GetMinValleyAmount()
-    {
-        return valleysEntityAmount[0];
-    }
-    public int GetMaxValleyAmount()
-    {
-        return valleysEntityAmount[1];
-    }
-    public int GetMinMountainAmount()
-    {
-        return mountainEntityAmount[0];
-    }
-    public int GetMaxMountainAmount()
-    {
-        return mountainEntityAmount[1];
-    }
-    public int GetMinFuelAmount()
-    {
-        return fuelEntityAmount[0];
-    }
-    public int GetMaxFuelAmount()
-    {
-        return fuelEntityAmount[1];
     }
 }
