db48188d08d994e4eca67906d1e333a0a27b6240
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2020-10-07 15:25:50 -0500

    Generation V1

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -8,6 +8,7 @@ public class ObjectGenerator : MonoBehaviour
     {
         GameObject blockGameObject = new GameObject("Block_" + id);
         Block _script = blockGameObject.AddComponent<Block>();
+        _script.id = id;
         return blockGameObject;
     }
     public static GameObject GenerateMountain(GameObject parent, int parentID, int myID, Vector3 initPos)
@@ -15,27 +16,34 @@ public class ObjectGenerator : MonoBehaviour
         GameObject mountainGameObject = new GameObject("Mountain_" + parentID + "_" + myID);
         mountainGameObject.transform.SetParent(parent.transform);
         Mountain _mountainScript = mountainGameObject.AddComponent<Mountain>();
+        _mountainScript.id = myID;
 
         GameObject EntryGameObject = new GameObject("Entry_" + myID);
         EntryGameObject.transform.SetParent(mountainGameObject.transform);
+        _mountainScript.Entry = EntryGameObject;
 
         GameObject TopGameObject = new GameObject("Top_" + myID);
         TopGameObject.transform.SetParent(mountainGameObject.transform);
+        _mountainScript.Top = TopGameObject;
 
         if (_mountainScript.exitWidth != 0)
         {
             GameObject ExitGameObject = new GameObject("Exit_" + myID);
-            TopGameObject.transform.SetParent(mountainGameObject.transform);
+            ExitGameObject.transform.SetParent(mountainGameObject.transform);
+            _mountainScript.Exit = ExitGameObject;
+
         }
 
         return mountainGameObject;
     }
 
-    public static GameObject GenerateValley(GameObject parent, int parentID, int myID, Vector3 initPosition)
+    public static GameObject GenerateValley(GameObject parent, int parentID, int myID, Vector3 initPosition, int width)
     {
         GameObject valleyGameObject = new GameObject("Valley_" + parentID + "_" + myID);
         valleyGameObject.transform.SetParent(parent.transform);
         Valley _valleyScript = valleyGameObject.AddComponent<Valley>();
+        _valleyScript.id = myID;
+        _valleyScript.actualWidth = width;
 
         return valleyGameObject;
     }
